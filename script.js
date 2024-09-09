d745b424f8954526c6f017d0c535dd3097d2e2ab
Henshaw <josephhensh2000@gmail.com>
2024-09-09 16:35:56 +0100

    More work

--- script.js
+++ script.js
@@ -22,3 +22,18 @@ menuTabs.addEventListener("click", (e) => {
         document.querySelector(controllee).classList.remove("hidden")
     }
 });
+
+const form = document.getElementById('form');
+form.addEventListener('submit', (e)=> {
+    e.preventDefault();
+    const username = document.getElementById('username').value;
+    const userEmail = document.getElementById('useremail').value;
+    const scheduledDate = document.getElementById('scheduled-date').value;
+    const scheduledTime = document.getElementById('scheduled-time').value;
+
+    console.log(username);
+    console.log(userEmail);
+    console.log(scheduledDate);
+    console.log(scheduledTime);
+    form.reset()
+})
