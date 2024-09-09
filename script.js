9b8628b8475a1d7f4c308a5dafc6ab0990121245
Henshaw <josephhensh2000@gmail.com>
2024-09-09 11:24:48 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+curtainNavBar = document.querySelector(".nav-bar");
+console.log(curtainNavBar);
+
+window.addEventListener("scroll", () => {
+    if (this.scrollY > 60) {
+        curtainNavBar.classList.add("curtain");
+    } else {
+        curtainNavBar.classList.remove("curtain");
+    }
+});
+
+const menuTabs = document.querySelector(".menu-tabs");
+menuTabs.addEventListener("click", (e) => {
+    // console.log(e.target);
+    if (e.target.classList.contains("menu-tab-item") && !e.target.classList.contains("active")){
+        const controllee = e.target.getAttribute("data-target");
+        menuTabs.querySelector(".active").classList.remove("active");
+        e.target.classList.add("active");
+        document.querySelector(".menu-tab-content.flex").classList.add("hidden");
+        document.querySelector(".menu-tab-content.flex").classList.remove("flex");
+        document.querySelector(controllee).classList.add("flex");
+        document.querySelector(controllee).classList.remove("hidden")
+    }
+});
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          'custom-bg': 'rgb(19, 18, 18)',
+        },
+      },
+    },
+    plugins: [],
+  }
+  
